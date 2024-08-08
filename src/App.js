5c91d0414b5b3af5c05eac7828073cb81a29edb5
Liburna Berisha <115898216+Liburna1@users.noreply.github.com>
2024-08-08 21:24:54 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.scss';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/shared/Navbar/Navbar';
-import Home from './pages/Home';
+import Home from './pages/Home/Home';
 import Footer from './components/shared/Footer/Footer';
 
 function App() {
   return (
     <Router>
-      <Navbar/>
+      <Navbar />
       <Routes>
-        <Route exact path='/' component={Home} />
+        <Route exact path='/' element={<Home />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </Router>
   );
 }
