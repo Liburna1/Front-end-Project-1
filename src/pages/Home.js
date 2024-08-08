dedcdf293e89bbd8c902f3b9c0f4313675466102
Liburna Berisha <115898216+Liburna1@users.noreply.github.com>
2024-08-08 21:23:42 +0200

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,54 @@
-import React from 'react'
-import Navbar from '../components/shared/Navbar/Navbar'
-const Home = () => {
-  return (
-    <div className='home-page'>
-      <Navbar/>
-    </div>
-  )
-}
+import React from 'react';
+import './Home.scss';
+import home from '../../assets/shared/home.jpg';
+import { data, modelOptions, locationOptions } from './data'; // Import all data
+import { FaMapMarked } from 'react-icons/fa';
 
-export default Home
+const Home = () => {
+  const section = data[0]; // Assuming data has only one set of images
+
+  return (
+    <div>
+      <div className="home-container">
+        <div className="background-overlay"></div>
+        <div className="image-container">
+          <img src={home} alt="Hire a Model or Become One" className="background-image" />
+          <div className="overlay">
+          <h1>HIRE A MODEL OR<br /><span className="indented-line">BECOME ONE</span></h1>
+            <div className="search-container">
+              <select className="dropdown">
+                {modelOptions.map((option) => (
+                  <option key={option.value} value={option.value}>
+                    {option.icon} {option.label}
+                  </option>
+                ))}
+              </select>
+              <select className="dropdown">
+                {locationOptions.map((option) => (
+                  <option key={option.value} value={option.value}>
+                   {option.icon} {option.label}
+                  </option>
+                ))}
+              </select>
+              <button className="find-model-button">Find model</button>
+            </div>
+          </div>
+        </div>
+        {section.images && (
+          <div className="images-menu">
+            {section.images.map((image, index) => (
+              <div
+                key={index}
+                className={`image-con ${image.position} ${image.rotate ? 'rotate' : ''}`}
+              >
+                <img src={image.src} alt={image.alt} className="menu-image" />
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
